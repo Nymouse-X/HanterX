40660bb1f663128fec105435e3468bbe4f83aa0d
NymouseX <saibaougokasu@gmail.com>
2021-11-02 23:45:11 +0700

    first commit

---
+++ hanter.sh
@@ -0,0 +1,268 @@
+clear
+echo "[>0%                        ]" | lolcat
+sleep 0.1
+clear
+echo "[>>5%                       ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>10%                     ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>15%                    ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>20%                   ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>25%                  ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>30%                 ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>35%                ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>>40%               ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>>>45%              ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>50%             ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>55%            ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>60%           ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>65%          ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>70%         ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>>>>75%      ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>>>>>80%     ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>>>>>>85%    ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>>>>>>>90%   ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>>>>>>>>95%  ]" | lolcat
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>>>>>>>>>100%]" | lolcat
+sleep 0.1
+clear
+echo "Cek Tools"
+apt install -y lolcat
+apy install -y toilet
+apt install -y figlet
+apt install -y bash
+sleep 1
+echo "Ok Semua Sudah Terinstall" | lolcat
+echo "Selamat Menjalankan Misi" | lolcat
+sleep 2
+clear
+sleep 2
+echo "
+██╗░░██╗░█████╗░███╗░░██╗████████╗███████╗██████╗░██╗░░██╗
+██║░░██║██╔══██╗████╗░██║╚══██╔══╝██╔════╝██╔══██╗╚██╗██╔╝
+███████║███████║██╔██╗██║░░░██║░░░█████╗░░██████╔╝░╚███╔╝░
+██╔══██║██╔══██║██║╚████║░░░██║░░░██╔══╝░░██╔══██╗░██╔██╗░
+██║░░██║██║░░██║██║░╚███║░░░██║░░░███████╗██║░░██║██╔╝╚██╗
+╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝" | lolcat
+sleep 1
+echo "***********************NYX*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************NYX*********************************"|lolcat
+sleep 1
+echo "nama kamu :"
+read -p " |NYX}--> " NM
+clear
+sleep 2
+toilet -f big -F gay $NM
+sleep 1
+echo "***********************$NM*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************$NM*********************************"|lolcat
+sleep 1
+echo "Pilih Teknik Haking" | lolcat
+echo 
+echo "1.Percepat Jaringan" | lolcat
+echo "2.Lacak IP/Link" | lolcat
+echo "3.Termux Tanpa Eror" | lolcat
+echo "4.Phising Sosmed" | lolcat
+echo "5.Hack Cctv Rendom" | lolcat
+echo "6.Exit" | lolcat
+echo
+read -p " |$NM}-->" PLH
+
+if [ $PLH = 1 ] || [ $PLH = 1 ]
+then
+clear
+echo "tunggu.." | lolcat
+sleep 0.1
+clear
+sleep 2
+toilet -f big -F gay $NM
+echo "***********************$NM*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************$NM*********************************"|lolcat
+sleep 1
+ping 1.1.1.1
+fi
+
+if [ $PLH = 2 ] || [ $PLH = 2 ]
+then
+clear
+echo "tunggu.." | lolcat
+sleep 0.1
+clear
+sleep 2
+toilet -f big -F gay $NM
+echo "***********************$NM*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************$NM*********************************"|lolcat
+sleep 1
+echo "Isi Target Kamu" | lolcat
+echo
+read -p "|$NM}-->" IP
+sleep 2
+echo "Sebentar Cek Tools Dulu" | lolcat
+apt install -y traceroute
+apt install -y tracepath
+apt insttall -y dnsuntil
+echo "Ok Semua Udah Langkap" | lolcat
+sleep 1
+clear
+toilet -f big -F gay $NM
+echo "***********************$NM*********************************" | lolcat
+traceroute $IP
+dig $IP
+fi
+
+if [ $PLH = 3 ] || [ $PLH = 3 ]
+then
+clear
+echo "tunggu.." | lolcat
+sleep 0.1
+clear
+sleep 2
+toilet -f big -F gay $NM
+echo "***********************$NM*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************$NM*********************************"|lolcat
+sleep 1
+echo "Sedang Proses" | lolcat
+pkg remove -y game-repo
+pkg remove -y science-repo
+pkg update 
+echo "Termux Kamu Udah Menjadi Terminal" | lolcat
+fi
+
+if [ $PLH = 4 ] || [ $PLH = 4 ]
+then
+clear
+echo "tunggu.." | lolcat
+sleep 0.1
+clear
+sleep 2
+toilet -f big -F gay $NM
+echo "***********************$NM*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************$NM*********************************"|lolcat
+sleep 1
+echo "Cek Tools" | lolcat
+apt install -y git
+git clone https://github.com/An0nUD4Y/blackeye.git ~/HanterX/Tools
+sleep 2
+clear
+echo "Pembuat Tools= }An0nUD4Y{" | lolcat
+cd ~/HanterX/Tools/blackeye
+chmod +x blackeye.sh
+bash ./blackeye.sh
+fi
+
+if [ $PLH = 5 ] || [ $PLH = 5 ]
+then
+clear
+echo "tunggu.." | lolcat
+sleep 0.1
+clear
+sleep 2
+toilet -f big -F gay $NM
+echo "***********************$NM*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************$NM*********************************"|lolcat
+sleep 1
+echo "Cek Tools" | lolcat
+apt install -y git
+apt install -y python3
+pip3 install requests
+pip3 install colorama
+git clone https://github.com/AngelSecurityTeam/Cam-Hackers ~/HanterX/Tools
+sleep 2
+clear
+echo "Pembuat Tools= }An0nUD4Y{" | lolcat
+cd ~/HanterX/Tools/Cam-Hackers
+python3 cam-hackers.py
+fi
+
+if [ $PLH = 6 ] || [ $PLH = 6 ]
+then
+clear
+echo "tunggu.." | lolcat
+sleep 0.1
+clear
+sleep 2
+toilet -f big -F gay $NM
+echo "***********************$NM*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************$NM*********************************"|lolcat
+sleep 1
+clear
+echo "
+███╗░░░███╗██╗░██████╗░██████╗██╗░█████╗░███╗░░██╗
+████╗░████║██║██╔════╝██╔════╝██║██╔══██╗████╗░██║
+██╔████╔██║██║╚█████╗░╚█████╗░██║██║░░██║██╔██╗██║
+██║╚██╔╝██║██║░╚═══██╗░╚═══██╗██║██║░░██║██║╚████║
+██║░╚═╝░██║██║██████╔╝██████╔╝██║╚█████╔╝██║░╚███║
+╚═╝░░░░░╚═╝╚═╝╚═════╝░╚═════╝░╚═╝░╚════╝░╚═╝░░╚══╝
+
+░█████╗░░█████╗░███╗░░░███╗██████╗░██╗░░░░░███████╗████████╗███████╗  ░░░░░░
+██╔══██╗██╔══██╗████╗░████║██╔══██╗██║░░░░░██╔════╝╚══██╔══╝██╔════╝  ░░░░░░
+██║░░╚═╝██║░░██║██╔████╔██║██████╔╝██║░░░░░█████╗░░░░░██║░░░█████╗░░  ░░░░░░
+██║░░██╗██║░░██║██║╚██╔╝██║██╔═══╝░██║░░░░░██╔══╝░░░░░██║░░░██╔══╝░░  ░░░░░░
+╚█████╔╝╚█████╔╝██║░╚═╝░██║██║░░░░░███████╗███████╗░░░██║░░░███████╗  ██╗██╗
+░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚═╝░░░░░╚══════╝╚══════╝░░░╚═╝░░░╚══════╝  ╚═╝╚═╝" | lolcat
+fi
+
+
+
+
+
